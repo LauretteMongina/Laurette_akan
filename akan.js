d216b6c25cb16a031b724524fb14af8eed720757
laurettemongina <monginalaurette2gmail.com>
2021-07-20 11:24:11 +0300

    Edits the js file.

--- akan.js
+++ akan.js
@@ -5,33 +5,30 @@ function getInput() {
   var month = parseInt(
       document.getElementById("month").value
       );
-      var year = parseInt(
+  var year = parseInt(
       document.getElementById("year").value);
-      var male = document.getElementById("maleR");
+  var male = document.getElementById("maleR");
       var female = document.getElementById("femaleR");
 
-      var dateOfBirth = new Date(year + "/" + month + "/" + date);
-      var weekDay = dateOfBirth.getDay();
+  var dateOfBirth = new Date(year + "/" + month + "/" + date);
+  var weekDay = dateOfBirth.getDay();
 
-      if (month < 1 || month > 12 || (month == 2 && date > 29)) {
+      if (month <= 0 || month > 12 || (month == 2 && date > 29)) {
           return alert("Enter a valid month!")}
-          else if (date < 1 || date > 31) {
+          else if (date <= 0 || date > 31) {
           return alert("Enter a valid date!");
           } 
-          else if (year === NaN || year < 1900 || year > 2100) {
-          return alert ("Enter a valid year");
-          }
-          var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+   var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-          var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+   var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
       
-          var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
        
       if (weekDay === 0 && male.checked) {
       return alert(
-          "Yikes! Your Akan name is  " +
+          "Yikes! Your Akan name is" +
           akanMale[weekDay] +
-          ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+          ("  .You were born on a " + daysOfTheWeek[weekDay]));
       } 
       else if (weekDay === 1 && male.checked) {
       return alert(
